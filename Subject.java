6425bd96619eaac02d80e5a299f930e7d4257d3e
Martin-GX <martin@goodx.co.za>
2026-05-06 09:16:35 +0200

    Add insertion sort for Test dates

--- Subject.java
+++ Subject.java
@@ -45,6 +45,25 @@ public class Subject implements Comparable<Subject>{
         Test nextTest = tests.peek();
         return (nextTest != null) ? nextTest.priority : 0;
     }
+
+    public void printInsertionSort() {
+        ArrayList<Test> tempTests = new ArrayList<>(this.tests);
+
+        for (int i = 1; i < tempTests.size(); i++) {
+            Test key = tempTests.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && tempTests.get(j).getTestDate().isAfter(key.getTestDate())) {
+                tempTests.set(j + 1, tempTests.get(j));
+                j = j - 1; 
+            }
+            tempTests.set(j + 1, key);
+        }
+
+        for (Test t : tempTests) {
+            System.out.println(t);
+        }
+    } 
     
     @Override
     public int compareTo(Subject other) {
